7a2ea4986c37467aa07b707b7ef43064a4af346d
jrehmke <75812381+jrehmke@users.noreply.github.com>
2021-02-25 20:08:01 -0800

    Art Animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-console.log('OK');
+// This changes everything
+
+// retrieve the element
+var element = document.getElementById("art1");
+
+// reset the transition by...
+element.addEventListener(
+  "click",
+  function (e) {
+    e.preventDefault;
+
+    // -> removing the class
+    element.classList.remove("arts");
+
+    //element.offsetWidth = element.offsetWidth;
+
+    void element.offsetWidth;
+
+    // -> and re-adding the class
+    element.classList.add("arts");
+  },
+  false
+);
